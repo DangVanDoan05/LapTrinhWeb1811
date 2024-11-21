8aed6578d1a606e74f85f3199a301e00e0ca2c73
Dang Van Doan 05 <dangvandoan0509@gmail.com>
2024-11-21 16:57:22 +0700

    Update 1 21-11-2024

---
+++ WebAPNet/WebBanHang/DTO/DanhMucDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DanhMucDTO
+    {
+        public DanhMucDTO(DataRow row)
+        {
+            this.ID = int.Parse(row["ID"].ToString());
+            this.TENDM = row["TENDM"].ToString();
+            this.MOTA = row["MOTA"].ToString();
+        }
+
+        private int iD;
+        private string tENDM;
+        private string mOTA;
+
+        public int ID { get => iD; set => iD = value; }
+        public string TENDM { get => tENDM; set => tENDM = value; }
+        public string MOTA { get => mOTA; set => mOTA = value; }
+
+
+    }
+}
---
+++ WebAPNet/WebBanHang/Web2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAO;
+using DTO;
+
+namespace Web2.Controllers
+{
+    public class HomeController : Controller // Nó đang là một Class.
+    {
+        // GET: Home
+        // Luôn chạy File Index đầu tiên.
+        // Như thế này thì sẽ hơi khó hiểu bản chất vấn đề.
+
+        public ActionResult Index()  // Đây là một phương thức.
+        {
+            List<DanhMucDTO> Ls = DanhMucDAO.Instance.GetListDM();
+            return View(Ls);
+        }
+      
+    }
+}
