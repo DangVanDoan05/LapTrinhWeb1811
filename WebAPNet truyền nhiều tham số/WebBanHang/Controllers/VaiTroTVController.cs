8733ace0d90b5ad92f4e4a07c8c42930867fa3b7
Dang Van Doan 05 <dangvandoan0509@gmail.com>
2024-12-06 17:27:20 +0700

    Update 1 06-12-2024

---
+++ WebAPNet truyền nhiều tham số/WebBanHang/Controllers/VaiTroTVController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DTO;
+using DAO;
+
+namespace WebBanHang.Controllers
+{
+    public class VaiTroTVController : Controller
+    {
+        // GET: VaiTroTV
+        public ActionResult Index()
+        {
+            List<DanhMucDTO> Ls = DanhMucDAO.Instance.GetListDM();
+            return View(Ls);
+        }
+    }
+}
